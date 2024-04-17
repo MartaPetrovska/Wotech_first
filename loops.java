db803b41af55cd5f0b7cca593492d9179a2809cb
Marta <133788310+MartaPetrovska@users.noreply.github.com>
2024-04-17 18:21:10 +0300

    Added 3rd homework WoTech.java

--- loops.java
+++ loops.java
@@ -93,3 +93,65 @@ public class Main {
 }
 
 
+/* Develop a program, that iterates through numbers from 0 till X amount of times (X is user provided)
+For numbers that are divided by 3: print out "Wo" .
+
+For numbers that are divided by 5: print out "Tech". 
+
+For numbers divided by 3 and 5: print out "WoTech".*/
+
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Enter a number");
+    Scanner input = new Scanner(System.in);
+    int iterations = input.nextInt();
+    
+    for (int i = 1; i <= iterations; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        System.out.println("WoTech");
+      } else if (i % 3 == 0) {
+        System.out.println("Wo");
+      } else if (i % 5 == 0) {
+        System.out.println("Tech");
+      } else {
+        System.out.println(i);
+      }
+    } 
+    
+    
+  }
+
+}
+
+
+
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please enter a number: ");
+
+    int number = scanner.nextInt(); //15
+
+    for(int i = 1; i <= number; i++){
+      // 1 2 3 4 5 ... 15
+      String result = "";
+      if (i % 3 == 0){ // 3 6 9 12 15
+        result += "Wo";
+      } 
+      if (i % 5 == 0){ // 5 10 15
+        result += "Tech";
+      } 
+      if(result.equals("")){ //if result is empty
+        result = String.valueOf(i); //Then result = i (number)
+      }
+      System.out.println(result);
+
+      scanner.close();
+    }
+  }
+}
