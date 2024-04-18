df1cfa6e8f5706dd6e93241737be558dd2e0ce21
Marta <133788310+MartaPetrovska@users.noreply.github.com>
2024-04-18 20:25:15 +0300

    Added 2nd task.java

--- arrays.java
+++ arrays.java
@@ -20,3 +20,34 @@ public class Main {
   }
 
 }
+
+
+// Find all the elements in the array that is below 0
+
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner arrayInput = new Scanner(System.in);  
+    
+    int[] array = new int[5];
+
+    System.out.println("Enter 5 numbers: ");
+    for (int i = 0; i < array.length; i++) {
+      array[i] = arrayInput.nextInt();
+    }
+
+    System.out.println("The numbers you entered are: ");
+    for (int i = 0; i < array.length; i++) {
+    System.out.println(array[i]);
+    }
+
+    System.out.println("Negative numbers from Your list are: ");
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] < 0)
+        System.out.println(array[i]); 
+    }
+
+    arrayInput.close();
+    
+  }
+}
