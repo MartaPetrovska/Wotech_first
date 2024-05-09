b9b789af8acb0761910a0275134dbf193dc15f23
Marta <133788310+MartaPetrovska@users.noreply.github.com>
2024-05-09 19:49:36 +0300

    Update multidimensionalArrays.java

--- multidimensionalArrays.java
+++ multidimensionalArrays.java
@@ -23,3 +23,33 @@ public class Main {
         }
     }
 }
+
+
+
+// Fill the 5x5 array with numbers from 1 to 25
+public class Main {
+    public static void main(String[] args) {
+        int[][] array = new int[5][5];
+        int startingValue = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            int[] row = array[i];
+              for (int j = 0; j < row.length; j++) {
+                row[j] = startingValue + 1;
+                startingValue ++;
+              }
+        }
+
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++){
+              if (array[i][j] < 10){
+                System.out.print(" " + array[i][j] + " ");
+              } else {
+                System.out.print(array[i][j] + " ");
+              }
+            }
+          System.out.println();
+        }
+    }
+}
+
