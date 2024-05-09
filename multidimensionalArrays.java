ad872da93c21297bd06afc84b25786e977e61bee
Marta <133788310+MartaPetrovska@users.noreply.github.com>
2024-05-09 19:55:44 +0300

    Update multidimensionalArrays.java

--- multidimensionalArrays.java
+++ multidimensionalArrays.java
@@ -53,3 +53,28 @@ public class Main {
     }
 }
 
+
+//Fill the array with random digits from 0 to 9
+public class Main {
+    public static void main(String[] args) {
+        int[][] array = new int[5][5];
+
+        for (int i = 0; i < array.length; i++) {
+            int[] row = array[i];
+              for (int j = 0; j < row.length; j++) {
+                row[j] = (int)(Math.random() * 101);
+              }
+        }
+
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++){
+              if (array[i][j] < 10){
+                System.out.print(" " + array[i][j] + " ");
+              } else {
+                System.out.print(array[i][j] + " ");
+              }
+            }
+          System.out.println();
+        }
+    }
+}
